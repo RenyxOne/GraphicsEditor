f16f395825eddc9779f49cc9d7508af3c65e34ba
Руслан Шамсутдинов <rrshamsutdinov@edu.tversu.ru>
2020-12-25 00:32:35 +0300

    add BorderDialog

--- src/menu/Decorator10.java
+++ src/menu/Decorator10.java
@@ -2,17 +2,6 @@ package menu;
 
 
 import mvc.controller.State;
-import mvc.model.BorderDecorator;
-import mvc.model.MyShape;
-import mvc.model.ShapeDecorator;
-import mvc.model.shapes.MyRectangle;
-import mvc.model.shapes.ShapeInterface;
-
-import java.awt.event.*;
-import javax.swing.*;
-
-import java.awt.*;
-import java.text.NumberFormat;
 
 public class Decorator10 implements Command{
     State state;
